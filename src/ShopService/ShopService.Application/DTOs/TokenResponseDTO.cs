f66011311258902b7a45c53edcb51786faaf33e4
EnesKllokoqi1 <eneskllokoqi@gmail.com>
2026-07-27 23:04:47 +0200

    feat: configure JWT bearer authentication in Program.cs,create DTOs,create UserRepository

---
+++ src/ShopService/ShopService.Application/DTOs/TokenResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopService.Application.DTOs
+{
+    public class TokenResponseDTO
+    {
+        public required string AccessToken { get; set; } = string.Empty;
+        public required string RefreshToken { get; set; } = string.Empty;
+    }
+}
