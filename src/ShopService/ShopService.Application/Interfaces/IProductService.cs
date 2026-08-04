748feddb9fa15ce84ef897e26dd4087881c701e7
EnesKllokoqi1 <eneskllokoqi@gmail.com>
2026-08-04 23:58:39 +0200

    feat:add methods to IProductService

--- src/ShopService/ShopService.Application/Interfaces/IProductService.cs
+++ src/ShopService/ShopService.Application/Interfaces/IProductService.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ShopService.Application.DTOs;
+using ShopService.Application.DTOs.ProductDTOs;
 
 namespace ShopService.Application.Interfaces
 {
     public interface IProductService
     {
-       
+        Task<List<ProductResponseDTO>> GetAllProducts();
+        Task<ProductResponseDTO> GetSpecificProduct(Guid guid );
+        Task<List<ProductCategoryResponseDTO>> GetProductCategories();
+        Task<ProductResponseDTO> CreateProduct(CreateProductDTO createProductDTO);
+        Task<ProductResponseDTO> UpdateProductDto(UpdateProductDTO createProductDTO);
+        Task<bool> DeleteProduct(Guid guid);
+        Task<bool> AdjustStockAsync(Guid productId, int quantityChange);
+        Task<List<ProductResponseDTO>> GetLowStockProductsAsync();
+        Task<List<ProductResponseDTO>> GetFeaturedProducts();
+        Task<ProductReviewResponseDTO> GetProductReviews(Guid guid);
     }
 }
