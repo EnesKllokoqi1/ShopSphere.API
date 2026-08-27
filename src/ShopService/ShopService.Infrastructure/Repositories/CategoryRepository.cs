da6c2a5a7f6256b3f1af934a1b9e8fa248c1fbe0
EnesKllokoqi1 <eneskllokoqi@gmail.com>
2026-08-27 23:24:53 +0200

    feat: add category service base files

---
+++ src/ShopService/ShopService.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using ShopService.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopService.Infrastructure.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+    }
+}
