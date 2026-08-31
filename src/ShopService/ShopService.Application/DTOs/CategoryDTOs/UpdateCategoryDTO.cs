cc0967aa4d9cb5d056d9e11cff8ddb17069dd933
EnesKllokoqi1 <eneskllokoqi@gmail.com>
2026-08-31 23:23:39 +0200

    feat: finish category DTO creations and create base ICategoryService methods

--- src/ShopService/ShopService.Application/DTOs/CategoryDTOs/UpdateCategoryDTO.cs
+++ src/ShopService/ShopService.Application/DTOs/CategoryDTOs/UpdateCategoryDTO.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ShopService.Application.DTOs.CategoryDTOs
 {
-    internal class UpdateCategoryDTO
+    public class UpdateCategoryDTO
     {
+        [Required(ErrorMessage = "Category name is required")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Category name must be between 2 and 100 characters")]
+        public string Name { get; set; } = string.Empty;
+
+        [StringLength(500, ErrorMessage = "Description cannot exceed 500 characters")]
+        public string? Description { get; set; }
+
+        public Guid? ParentCategoryId { get; set; }
+
+        [Range(0, 999, ErrorMessage = "Display order must be between 0 and 999")]
+        public int DisplayOrder { get; set; }
+
+        public bool IsActive { get; set; }
     }
 }
--- src/ShopService/ShopService.Application/Interfaces/ICategoryService.cs
+++ src/ShopService/ShopService.Application/Interfaces/ICategoryService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ShopService.Application.DTOs.CategoryDTOs;
+using ShopService.Application.DTOs.ProductDTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,12 @@ using System.Threading.Tasks;
 
 namespace ShopService.Application.Interfaces
 {
-    internal interface ICategoryService
+    public interface ICategoryService
     {
+        Task<CategoryResponseDTO?> CreateCategory(CreateCategoryDTO createCategoryDTO);
+        Task<CategoryResponseDTO?> UpdateCategory(UpdateCategoryDTO updatedCategory,Guid categoryId);
+        Task<bool> DeleteCategory(Guid guid);
+        Task<IEnumerable<CategoryResponseDTO>> GetAllCategories();
+        Task<CategoryResponseDTO?> GetCategoryById(Guid categoryId);
     }
 }
