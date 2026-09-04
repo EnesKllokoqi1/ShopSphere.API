90e7c7201f840200fbf8666e82de313e8fad7764
EnesKllokoqi1 <eneskllokoqi@gmail.com>
2026-09-04 23:45:42 +0200

    chore: create base files for reviews feature

---
+++ src/ShopService/ShopService.API/Controllers/ReviewController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+    }
+}
--- src/ShopService/ShopService.API/Program.cs
+++ src/ShopService/ShopService.API/Program.cs
@@ -24,6 +24,8 @@ builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IReviewRepository,ReviewRepository>();
+builder.Services.AddScoped<IReviewService, ReviewService>();
 builder.Services.AddOpenApi();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
