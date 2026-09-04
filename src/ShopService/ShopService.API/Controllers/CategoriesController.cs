130cb81ed600ea2fb52d6a2bb3ca3f2c5747187b
EnesKllokoqi1 <eneskllokoqi@gmail.com>
2026-09-04 20:31:33 +0200

    feat: implement CategoriesController and register category dependencies

--- src/ShopService/ShopService.API/Controllers/CategoriesController.cs
+++ src/ShopService/ShopService.API/Controllers/CategoriesController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ShopService.Application.DTOs.CategoryDTOs;
+using ShopService.Application.Interfaces;
+using ShopService.Domain.Entities;
 
 namespace ShopService.API.Controllers
 {
@@ -7,5 +11,70 @@ namespace ShopService.API.Controllers
     [ApiController]
     public class CategoriesController : ControllerBase
     {
+        private readonly ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [Authorize(Policy = "AdminOnly")]
+        [HttpPost]
+        public async Task<ActionResult<CategoryResponseDTO>> CreateCategory([FromBody] CreateCategoryDTO createCategoryDTO)
+        {
+            var category = await _categoryService.CreateCategory(createCategoryDTO);
+            if (category is null)
+            {
+                return Conflict(new
+                {
+                    Message = "There already exists a category with that name"
+                });
+            }
+            return CreatedAtAction(
+                 nameof(GetCategoryById),
+                 new { categoryId = category.Id },
+                 category
+             );
+        }
+        [Authorize(Policy = "AdminOnly")]
+        [HttpPut("{categoryId:guid}")]
+        public async Task<ActionResult<CategoryResponseDTO>> UpdateCategory([FromBody] UpdateCategoryDTO updateCategoryDTO,[FromRoute] Guid categoryId)
+        {
+            var updatedCategory = await _categoryService.UpdateCategory(updateCategoryDTO, categoryId);
+            if (updatedCategory is null)
+            {
+                return NotFound(new
+                {
+                    Message = "Category with that Id has not been found"
+                });
+            }
+            return Ok(updatedCategory);
+        }
+        [Authorize(Policy = "AdminOnly")]
+        [HttpDelete("{categoryId:guid}")]
+        public async Task<ActionResult> DeleteCategory([FromRoute] Guid categoryId)
+        {
+            var check = await _categoryService.DeleteCategory(categoryId);
+            if (!check)
+            {
+                return NotFound(new { Message= "Category with that Id has not been found" });
+            }
+            return NoContent();
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryResponseDTO>>> GetAllCategories()
+        {
+            var categories = await _categoryService.GetAllCategories();
+            return Ok(categories);
+        }
+        [HttpGet("{categoryId:guid}")]
+        public async Task<ActionResult<CategoryResponseDTO>> GetCategoryById([FromRoute] Guid categoryId)
+        {
+            var category = await _categoryService.GetCategoryById(categoryId);
+            if (category is null)
+            {
+                return NotFound(new { Message = "Category with that Id not found" });
+            }
+            return Ok(category);
+        }
     }
 }
