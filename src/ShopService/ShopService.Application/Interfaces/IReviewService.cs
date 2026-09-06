6588a2f479c6ba1db29efdc79bf270f21cb28025
EnesKllokoqi1 <eneskllokoqi@gmail.com>
2026-09-06 22:33:38 +0200

    feat: implement paginated review service with repository refactor

--- src/ShopService/ShopService.Application/Interfaces/IReviewService.cs
+++ src/ShopService/ShopService.Application/Interfaces/IReviewService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShopService.Application.DTOs.ReviewDTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace ShopService.Application.Interfaces
 {
     public interface IReviewService
     {
+        Task<ReviewResponseDTO?> CreateReviewAsync(MakeReviewDTO makeReviewDTO);
+        Task<ReviewResponseDTO?> UpdateReviewAsync(UpdateReviewDTO updateReviewDTO,Guid reviewId);
+        Task<bool> DeleteReviewAsync(Guid reviewId);
+        Task<IEnumerable<ReviewResponseDTO>> GetAllReviewsAsync(int pageNumber,int pageSize);
+        Task<ReviewResponseDTO?> GetReviewByIdAsync(Guid guid);
+        Task<IEnumerable<ReviewResponseDTO>> GetReviewsByUserIdAsync(Guid userId);
     }
 }
