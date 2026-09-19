8eff6e62389abb96d79c7cfba78c5b9a4af9b0c6
EnesKllokoqi1 <eneskllokoqi@gmail.com>
2026-09-19 23:29:15 +0200

    feat:add many-to-many relation between Coupon and Product

---
+++ src/ShopService/ShopService.Infrastructure/Migrations/20260919211824_AddCouponProducts.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCouponProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CouponProduct",
+                columns: table => new
+                {
+                    CouponsId = table.Column<Guid>(type: "uuid", nullable: false),
+                    productsId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CouponProduct", x => new { x.CouponsId, x.productsId });
+                    table.ForeignKey(
+                        name: "FK_CouponProduct_Coupons_CouponsId",
+                        column: x => x.CouponsId,
+                        principalTable: "Coupons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CouponProduct_Products_productsId",
+                        column: x => x.productsId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CouponProduct_productsId",
+                table: "CouponProduct",
+                column: "productsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CouponProduct");
+        }
+    }
+}
